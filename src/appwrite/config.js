18c52969c228a6b7a9978ee3e663a50b71c24a19
vedant pandey <pvedant69@gmail.com>
2024-03-02 13:31:07 +0530

    crud operations completed

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -30,6 +30,84 @@ export class Service{
             console.log("")
         }
     }
+    async updatePost(slug,{title,content,featuredImage,status}){
+         try {
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status
+                }
+            )
+         } catch (error) {
+            console.log("")
+         }
+    }
+    async deletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+            return true;
+        } catch (error) {
+            console.log("Appwrite service :: deletePost :: error",error);
+            return false;
+        }
+    }
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+        } catch (error) {
+            console.log("Appwrite service :: getPost :: error",error);
+        }
+    }
+    async getPosts(queries=[Query.equal("status","active")]){
+       try {
+        return await this.databases.listDocuments(
+            conf.appwriteDatabaseId,
+            conf.appwriteCollectionId,
+            queries,
+        )
+       } catch (error) {
+        console.log("Appwrite service :: getPost :: error",error);
+       }
+    }
+    //file upload
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite service :: uploadFile :: error",error);
+        }
+    }
+    async deleteFile(fileID){
+        try {
+            await this.bucket.deleteFile(conf.appwriteBucketId,
+                fileID)
+                return true;
+        } catch (error) {
+            console.log("Appwrite service :: getPost :: error",error);
+            return false
+        }
+    }
+    getFilePreview(fileID){
+        conf.appwriteBucketId,
+        fileID
+    }
 }
 
 
