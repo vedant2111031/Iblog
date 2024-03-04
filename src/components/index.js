7c11b52731187eeb02046ec79ab180dfc867a81c
vedant pandey <pvedant69@gmail.com>
2024-03-05 00:49:06 +0530

    finalizing

--- src/components/index.js
+++ src/components/index.js
@@ -3,10 +3,12 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn"
+import RTE from "./RTE";
 export{
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE
 }
