0b9a3ed502827b0da6bdd0960fc822adcb0ada9b
vedant pandey <pvedant69@gmail.com>
2024-03-04 23:50:44 +0530

    login signup done

--- src/components/Postcards.jsx
+++ src/components/Postcards.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import appwriteService from "../appwrite/config"
-import {Link} from 'react-roucter-dom'
+import {Link} from 'react-router-dom'
 
-
-function Postcards({$id,title,featuredImage}) {
+function PostCard({$id, title, featuredImage}) {
+    
   return (
     <Link to={`/post/${$id}`}>
-        
+        <div className='w-full bg-gray-100 rounded-xl p-4'>
+            <div className='w-full justify-center mb-4'>
+                <img src={appwriteService.getFilePreview(featuredImage)} alt={title}
+                className='rounded-xl' />
+
+            </div>
+            <h2
+            className='text-xl font-bold'
+            >{title}</h2>
+        </div>
     </Link>
   )
 }
 
-export default Postcards
+
+export default PostCard
